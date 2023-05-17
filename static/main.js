4f7ddb5439a351e8dedc564fe0c8e3f220e09899
bongho <wqoo2190@gmail.com>
2023-05-17 20:42:42 +0900

    html css slide적용을 위해 전체적인 수정

---
+++ static/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  slidesPerView: 'auto',
+  loop: true,
+  autoplay: {
+      delay: 2000,
+    },
+  speed: 900,
+})
