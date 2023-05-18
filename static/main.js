0d15b0d813eefc47c635d214cd010f5c22387147
bongho <wqoo2190@gmail.com>
2023-05-18 11:17:52 +0900

    amend modal in html,css,js

--- static/main.js
+++ static/main.js
@@ -8,3 +8,19 @@ const swiper = new Swiper('.swiper', {
     },
   speed: 900,
 })
+
+const modal = document.getElementById("popup01")
+const btnModal = document.getElementById("openModalBtn")
+btnModal.addEventListener("click", e => {
+  modal.style.display = "flex"
+
+})
+const closeBtn = document.getElementById("modal-btn")
+closeBtn.addEventListener("click", e => {
+    modal.style.display = "none"
+})
+window.addEventListener("keyup", e => {
+  if(modal.style.display === "flex" && e.key === "Escape") {
+      modal.style.display = "none"
+  }
+})
