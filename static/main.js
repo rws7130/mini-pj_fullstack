a902cd4b710a7b4266f2d820b1e270b66c23fb2b
bongho <wqoo2190@gmail.com>
2023-05-18 15:52:27 +0900

    post

--- static/main.js
+++ static/main.js
@@ -4,8 +4,8 @@ const swiper = new Swiper('.swiper', {
   slidesPerView: 'auto',
   loop: true,
   autoplay: {
-      delay: 2000,
-    },
+    delay: 2000,
+  },
   speed: 900,
 })
 
@@ -17,10 +17,41 @@ btnModal.addEventListener("click", e => {
 })
 const closeBtn = document.getElementById("modal-btn")
 closeBtn.addEventListener("click", e => {
-    modal.style.display = "none"
+  modal.style.display = "none"
 })
 window.addEventListener("keyup", e => {
-  if(modal.style.display === "flex" && e.key === "Escape") {
-      modal.style.display = "none"
+  if (modal.style.display === "flex" && e.key === "Escape") {
+    modal.style.display = "none"
   }
 })
+
+$(document).ready(function () {
+  show_info();
+});
+
+function save_info() {
+  let name = $('#noplan').val()
+  let best = $('#best').val()
+  let style = $('#style').val()
+  let blog = $('#blog').val()
+  let image = $('#nimage').val()
+
+  let formData = new FormData();
+  formData.append("name_give", name)
+  formData.append("best_give", best)
+  formData.append("style_give", style)
+  formData.append("blog_give", blog)
+  formData.append("image_give", image)
+
+  fetch('/infom', { method: "POST", body: formData }).then((res) => res.json()).then((data) => {
+    alert(data["msg"]);
+    window.location.reload()
+  })
+}
+
+function show_info() {
+  fetch ('/info').then((res) => res.json()).then((data) => {
+    let rows = data['result']
+    console.log(rows)
+  })
+}
