d7fde550dd9ee6ac0a4f64e20cdcfdc419833374
unknown <song>
2023-05-18 16:49:10 +0900

    서브페이지 GET요청/ app, sub 파일 수정 sub.css 파일 생성

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+# 몽고 url 입력
 from pymongo import MongoClient
 import certifi
 ca = certifi.where()
-client = MongoClient('mongodb+srv://sparta:test@cluster0.hjivimn.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient('...', tlsCAFile=ca)
 db = client.dbsparta
 
 @app.route('/')
@@ -13,7 +14,13 @@ def home():
 
 @app.route('/sub')
 def mypage():  
-   return 'This is My Page!'
+   return render_template('sub.html')
+
+# 서브페이지 GET 요청
+@app.route("/sub/team", methods=["GET"])
+def mypage_get():
+    all_teams = list(db.teams.find({},{'_id':False}))
+    return jsonify({'result': all_teams})
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
